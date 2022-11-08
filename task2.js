f873df171feacd9e746c7252d5221f241193659c
remiyeku <antakin4@gmail.com>
2022-11-08 12:14:14 +0000

    task 1 & 2 completed

--- task2.js
+++ task2.js
@@ -1,11 +1,15 @@
 let animals = [
-    "baboon",
-    "kangaroo",
-    "rhino",
-    "frog",
-    "beaver",
-    "horse",
-    "basalisk",
-  ];
-  
-let bListAnimals = // Your 'filter' code here.
+  "baboon",
+  "kangaroo",
+  "rhino",
+  "frog",
+  "beaver",
+  "horse",
+  "basalisk",
+];
+
+let bListAnimals = animals.filter(bAnimals);
+
+function bAnimals(animal) {
+  return animal[0] === "b";
+}
