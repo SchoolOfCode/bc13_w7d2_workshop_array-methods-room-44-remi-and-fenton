cf8d9b711c354f1f72f8f8dcfb78cd8122a15894
FentonPlusPlus <fentonhutson@gmail.com>
2022-11-08 12:29:52 +0000

    Task 3 in progress

--- task3.js
+++ task3.js
@@ -1,1 +1,24 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+let three = 3;
+let five = 5;
+let sixty = 60;
+let ninety = 90;
+
+const firstSort = someNumbers.some(function () {
+    return number % three === 0 || number % five === 0 || number % sixty === 0 || number % ninety === 0;
+})
+
+function isMultiple(number) {
+    return number % three === 0 || number % five === 0 || number % sixty === 0 || number % ninety === 0;
+};
+
+const secondSort = someNumbers.some(isMultiple);
+
+
+// const numbers {
+//     three: 3,
+//     five: 5,
+//     sixty: 60,
+//     ninety: 90
+// }
